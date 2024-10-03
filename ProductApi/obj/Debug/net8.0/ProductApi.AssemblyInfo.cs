1891785d5a61feb1b8dfc06aa8c9c87518c7c6bd
Farkas <farasz@kkszki.hu>
2024-10-03 12:20:59 +0200

    Get endpoint is ready.

--- ProductApi/obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
+++ ProductApi/obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+345181b981d3f20be20c35153c1791eb8f67e55c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
